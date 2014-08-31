7b7ab03f2eea8fb3e372702be622224a4b04a935
Veronica Rivera <vrivera@hmc.edu>
2014-08-31 15:34:15 -0700

    Wrote a check to see if a room is open
    Summary: I wrote an async method to check whether a room is open. It connects to the mongo database

--- app.js
+++ app.js
@@ -1,5 +1,31 @@
 var express = require('express');
 var server = express();
+var MongoClient = require('mongodb').MongoClient
+
+function isOpen(roomNumber, callback) {
+	MongoClient.connect('mongodb://127.0.0.1:27017/shanahan_rooms', function(err,db) {
+
+		var collection = db.collection('time_blocks');
+		collection.find({room:roomNumber}).toArray(function (err, results){ 
+			for(var i = 0; i < results.length; i++){
+				var date = new Date();
+				var hour = date.getHours();
+				var mins = date.getMinutes();
+				var block = results[i];
+				if(hour >= block.start_hour && hour <= block.end_hour &&
+					mins >= block.start_min && mins<= block.end_min){
+					callback(false);
+					db.close();
+					return;
+				}
+			}
+			callback(true);
+			db.close();
+			return;
+		})
+	})
+
+}
 
 server.get(/^\/room\/(\w+)$/, function (req, res) {
 	var roomNumber = req.params[0];
