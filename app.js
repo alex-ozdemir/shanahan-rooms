b2e6dd203c159a5d1a6af354b7bc5e9eaa8d8f59
Veronica Rivera <vrivera@hmc.edu>
2014-08-31 16:48:00 -0700

    Handled Wraparound time ranges.
    Implemented another method which checks wrap-around time ranges using the existing time comparison methods.

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ var express = require('express');
 var server = express();
 var MongoClient = require('mongodb').MongoClient
 
+function isTimeInRangeWrap(hour, min, start_hour, start_min, end_hour, end_min) {
+	if (isTimeAfter(start_hour, start_min, end_hour, end_min))	return !isTimeInRange(hour, min, start_hour, start_min, end_hour, end_min);
+	else isTimeInRange(hour, min, start_hour, start_min, end_hour, end_min);
+}
 
 function isTimeInRange(hour, min, start_hour, start_min, end_hour, end_min) {
 	var after = isTimeAfter(hour, min, start_hour, start_min);
@@ -22,13 +26,13 @@ function isOpen(roomNumber, callback) {
 	MongoClient.connect('mongodb://127.0.0.1:27017/shanahan_rooms', function(err,db) {
 
 		var collection = db.collection('time_blocks');
-		collection.find({room:roomNumber}).toArray(function (err, results){ 
+		collection.find({room: roomNumber}).toArray(function (err, results){ 
 			for(var i = 0; i < results.length; i++){
 				var date = new Date();
 				var hour = date.getHours();
 				var mins = date.getMinutes();
 				var block = results[i];
-				if (isTimeInRange(hour, mins, block.start_hour, block.start_min, block.end_hour, block.end_min)) {
+				if (isTimeInRangeWrap(hour, mins, block.start_hour, block.start_min, block.end_hour, block.end_min)) {
 					callback(false);
 					db.close();
 					return;
