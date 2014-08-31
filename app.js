3ab354b54c484a129fe1daf6b7ab335bf1c8855e
Alex Ozdemir <aozdemir@hmc.edu>
2014-08-31 16:11:27 -0700

    Fixed logic on comparing times.
    Wrote a set of functions which can evaluate whethera time (as defined by a
    hour and minute) is after another time, before another time, or between
    two times.
    
    Tested moderately.

--- app.js
+++ app.js
@@ -2,6 +2,22 @@ var express = require('express');
 var server = express();
 var MongoClient = require('mongodb').MongoClient
 
+
+function isTimeInRange(hour, min, start_hour, start_min, end_hour, end_min) {
+	var after = isTimeAfter(hour, min, start_hour, start_min);
+	var before = isTimeBefore(hour, min, end_hour, end_min);
+	return after && before;
+}
+
+function isTimeAfter(hour, min, ref_hour, ref_min) {
+	if (hour != ref_hour) return (hour > ref_hour);
+	else return (min >= ref_min);
+}
+function isTimeBefore(hour, min, ref_hour, ref_min) {
+	if (hour != ref_hour) return (hour < ref_hour);
+	else return (min <= ref_min);
+}
+
 function isOpen(roomNumber, callback) {
 	MongoClient.connect('mongodb://127.0.0.1:27017/shanahan_rooms', function(err,db) {
 
@@ -12,8 +28,7 @@ function isOpen(roomNumber, callback) {
 				var hour = date.getHours();
 				var mins = date.getMinutes();
 				var block = results[i];
-				if(hour >= block.start_hour && hour <= block.end_hour &&
-					mins >= block.start_min && mins<= block.end_min){
+				if (isTimeInRange(hour, mins, block.start_hour, block.start_min, block.end_hour, block.end_min)) {
 					callback(false);
 					db.close();
 					return;
@@ -29,7 +44,10 @@ function isOpen(roomNumber, callback) {
 
 server.get(/^\/room\/(\w+)$/, function (req, res) {
 	var roomNumber = req.params[0];
-	res.send(String(roomNumber));
+	isOpen(roomNumber, function (isOpen) {
+		if(isOpen) res.send("The room is open!");
+		else res.send("The room is closed");
+	});
 });
 
 server.listen(80);
