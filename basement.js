f6ff41e09a8f24dd174ead226c8a1d19abe14971
Alex Ozdemir <aozdemir@hmc.edu>
2014-09-06 15:53:01 -0700

    Added Veronica's dimensions for the bottom rooms.

--- basement.js
+++ basement.js
@@ -34,7 +34,33 @@ function fillB445(color) {
 	var y = 291;
 	var w = 60;
 	var h = 46;
-	var h = 46;
+    context.rect(x, y, w, h);
+	context.fill();
+}
+
+function fill1430(color) {
+	context = thisContext(color)
+	var x = 213;
+	var y = 213;
+	var w = 111;
+	var w2 = 121;
+	var h = 79;
+	context.beginPath();
+    context.moveTo(x, y);
+    context.lineTo(x + w, y);
+    context.lineTo(x + w2, y + h);
+    context.lineTo(x, y + h);
+    context.closePath()
+	context.fill();
+}
+
+function fillB450(color) {
+	context = thisContext(color)
+	context.beginPath();
+	var x = 89;
+	var y = 354;
+	var w = 45;
+	var h = 57;
     context.rect(x, y, w, h);
 	context.fill();
 }
