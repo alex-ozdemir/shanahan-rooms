cfea54d92da1fb71f4fe334641eb7af66beade94
Alex Ozdemir <aozdemir@hmc.edu>
2014-09-06 15:18:10 -0700

    Added a URL for getting a list of open rooms
    /rooms/

--- app.js
+++ app.js
@@ -25,38 +25,93 @@ function isTimeBefore(hour, min, ref_hour, ref_min) {
 	else return (min <= ref_min);
 }
 
-function isOpen(roomNumber, callback) {
+// function isOpen(roomNumber) {
+// 	MongoClient.connect('mongodb://127.0.0.1:27017/shanahan_rooms', function(err,db) {
+
+// 		var collection = db.collection('time_blocks');
+// 		collection.find({room: roomNumber}).toArray(function (err, results){ 
+// 			for(var i = 0; i < results.length; i++){
+// 				var date = new Date();
+// 				var hour = date.getHours();
+// 				var mins = date.getMinutes();
+// 				console.log("Time: " + hour + ":" + mins);
+// 				var block = results[i];
+// 				console.log("Block: " + block.start_hour + ":" + block.start_min + " - " + block.end_hour + ":" + block.end_min);
+// 				if (isTimeInRangeWrap(hour, mins, block.start_hour, block.start_min, block.end_hour, block.end_min)) {
+// 					callback(false);
+// 					db.close();
+// 					return;
+// 				}
+// 			}
+// 			callback(true);
+// 			db.close();
+// 			return;
+// 		})
+// 	})
+// }
+
+function getAllBlocks(callback) {
 	MongoClient.connect('mongodb://127.0.0.1:27017/shanahan_rooms', function(err,db) {
 
 		var collection = db.collection('time_blocks');
-		collection.find({room: roomNumber}).toArray(function (err, results){ 
-			for(var i = 0; i < results.length; i++){
-				var date = new Date();
-				var hour = date.getHours();
-				var mins = date.getMinutes();
-				console.log("Time: " + hour + ":" + mins);
-				var block = results[i];
-				console.log("Block: " + block.start_hour + ":" + block.start_min + " - " + block.end_hour + ":" + block.end_min);
-				if (isTimeInRangeWrap(hour, mins, block.start_hour, block.start_min, block.end_hour, block.end_min)) {
-					callback(false);
-					db.close();
-					return;
-				}
-			}
-			callback(true);
-			db.close();
-			return;
+		collection.find().toArray(function (err, results) {
+			callback(results);
 		})
 	})
+}
 
+function getRooms(blocks) {
+	var rooms = [];
+	for (var i = 0; i < blocks.length; i++)
+		if (rooms.indexOf(blocks[i].room) == -1) 
+			rooms.push(blocks[i].room);
+	return rooms
+}
+
+function getBlocksByRoom(room, callback) {
+	MongoClient.connect('mongodb://127.0.0.1:27017/shanahan_rooms', function(err, db) {
+
+		var collection = db.collection('time_blocks');
+		collection.find({room: room}).toArray(function (err, results) {
+			callback(results);
+		})
+	})
+}
+
+function isOpen(room, blocks) {
+	for (var i = 0; i < blocks.length; i++) {
+		var block = blocks[i];
+		if (block.room == room) {
+			var date = new Date();
+			var hour = date.getHours();
+			var mins = date.getMinutes();
+ 			console.log("Time: " + hour + ":" + mins);
+			console.log("Block: " + block.start_hour + ":" + block.start_min + " - " + block.end_hour + ":" + block.end_min);
+			if (isTimeInRangeWrap(hour, mins, block.start_hour, block.start_min, block.end_hour, block.end_min))
+				return false;
+		}
+	}
+	return true;
 }
 
 server.get(/^\/room\/(\w+)$/, function (req, res) {
-	var roomNumber = req.params[0];
-	isOpen(roomNumber, function (isOpen) {
-		if(isOpen) res.send("The room is open!");
+	var room = req.params[0];
+	getBlocksByRoom(room, function (blocks) {
+		if (isOpen(room, blocks)) res.send("The room is open!");
 		else res.send("The room is closed");
 	});
+});
+
+server.get('/rooms/', function (req, res) {
+	getAllBlocks(function (blocks) {
+		var rooms = getRooms(blocks);
+		var msg = '<ul>';
+		for (var i = 0; i < rooms.length; i++) {
+			if (isOpen(rooms[i], blocks)) msg += "<li>" + rooms[i] + "</li>";
+		}
+		msg += '<ul>'
+		res.send("Open rooms: <br />" + msg);
+	})
 });
 
 server.listen(80);
