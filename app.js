dc5079dc02cefc386de622ebab553e2479fd926a
Alex Ozdemir <aozdemir@hmc.edu>
2014-11-26 22:57:53 -0800

    Switched to JS Date objects rather than individually tracking hours and minutes

--- app.js
+++ app.js
@@ -2,53 +2,12 @@ var express = require('express');
 var server = express();
 var MongoClient = require('mongodb').MongoClient
 
-function isTimeInRangeWrap(hour, min, start_hour, start_min, end_hour, end_min) {
-	if (isTimeAfter(start_hour, start_min, end_hour, end_min))	{
-		console.log("Inverted Range");
-		return !isTimeInRange(hour, min, end_hour, end_min, start_hour, start_min);
-	}
-	else return isTimeInRange(hour, min, start_hour, start_min, end_hour, end_min);
+function normDate(date) {
+	std = new Date("January 5, 2014");
+	weeksLater = Math.floor( (date - std) / 1000 / 3600 / 24 / 7);
+	date.setDate(date.getDate() - weeksLater * 7);
+	return date;
 }
-
-function isTimeInRange(hour, min, start_hour, start_min, end_hour, end_min) {
-	var after = isTimeAfter(hour, min, start_hour, start_min);
-	var before = isTimeBefore(hour, min, end_hour, end_min);
-	return after && before;
-}
-
-function isTimeAfter(hour, min, ref_hour, ref_min) {
-	if (hour != ref_hour) return (hour > ref_hour);
-	else return (min >= ref_min);
-}
-function isTimeBefore(hour, min, ref_hour, ref_min) {
-	if (hour != ref_hour) return (hour < ref_hour);
-	else return (min <= ref_min);
-}
-
-// function isOpen(roomNumber) {
-// 	MongoClient.connect('mongodb://127.0.0.1:27017/shanahan_rooms', function(err,db) {
-
-// 		var collection = db.collection('time_blocks');
-// 		collection.find({room: roomNumber}).toArray(function (err, results){ 
-// 			for(var i = 0; i < results.length; i++){
-// 				var date = new Date();
-// 				var hour = date.getHours();
-// 				var mins = date.getMinutes();
-// 				console.log("Time: " + hour + ":" + mins);
-// 				var block = results[i];
-// 				console.log("Block: " + block.start_hour + ":" + block.start_min + " - " + block.end_hour + ":" + block.end_min);
-// 				if (isTimeInRangeWrap(hour, mins, block.start_hour, block.start_min, block.end_hour, block.end_min)) {
-// 					callback(false);
-// 					db.close();
-// 					return;
-// 				}
-// 			}
-// 			callback(true);
-// 			db.close();
-// 			return;
-// 		})
-// 	})
-// }
 
 function getAllBlocks(callback) {
 	MongoClient.connect('mongodb://127.0.0.1:27017/shanahan_rooms', function(err,db) {
@@ -79,15 +38,15 @@ function getBlocksByRoom(room, callback) {
 }
 
 function isOpen(room, blocks) {
+	now = normDate(new Date());
+	console.log(blocks);
 	for (var i = 0; i < blocks.length; i++) {
 		var block = blocks[i];
 		if (block.room == room) {
-			var date = new Date();
-			var hour = date.getHours();
-			var mins = date.getMinutes();
- 			console.log("Time: " + hour + ":" + mins);
-			console.log("Block: " + block.start_hour + ":" + block.start_min + " - " + block.end_hour + ":" + block.end_min);
-			if (isTimeInRangeWrap(hour, mins, block.start_hour, block.start_min, block.end_hour, block.end_min))
+			start = new Date(block.start);
+			end = new Date(block.end);
+			console.log(start, end);
+			if (now > start && now < end)
 				return false;
 		}
 	}
@@ -113,5 +72,21 @@ server.get('/rooms/', function (req, res) {
 		res.send("Open rooms: <br />" + msg);
 	})
 });
+
+server.get(/^\/block\/(\w*)\/([^\/]*)\/([^\/]*)\/$/, function (req, res) {
+	var room = req.params[0];
+	var start = normDate(new Date(req.params[1])).toJSON();
+	var end = normDate(new Date(req.params[2])).toJSON();
+	console.log("Start: " + req.params[1]);
+	console.log("  End: " + req.params[2]);
+	console.log("Start: " + start);
+	console.log("  End: " + end);
+	MongoClient.connect('mongodb://127.0.0.1:27017/shanahan_rooms', function(err, db) {
+		var collection = db.collection('time_blocks');
+		collection.insert({room: room, start: start, end: end}, function(err, rec) {
+			res.send("Insert complete, id: " + rec[0]._id);
+		})
+	})
+})
 
 server.listen(80);
